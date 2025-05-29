9516fd4baf270015a8e824efcc2e7d1963617020
FRANCISCO MENEZES <franciscomenezesbh@gmail.com>
2025-05-29 11:22:07 -0300

    componente login

---
+++ src/app/authentication/_components/sing-in-form.tsx
@@ -0,0 +1,104 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const singInSchema = z.object({
+  email: z.string().trim().email({ message: 'Email inválido' }),
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: 'Senha deve ter pelo menos 8 caracteres' }),
+})
+
+const SingInForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof singInSchema>>({
+    resolver: zodResolver(singInSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof singInSchema>) {
+    console.log(values)
+  }
+  return (
+    <Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Faça login para continuar.</CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email:</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Informe seu email"
+                      {...field}
+                      type="email"
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha:</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Informe sua senha"
+                      {...field}
+                      type="password"
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button type="submit" className="w-full">
+              Entrar
+            </Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  )
+}
+
+export default SingInForm
--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,53 +1,9 @@
-'use client'
-import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
 
-const registerSchema = z.object({
-  name: z.string().trim().min(1, { message: 'Nome é obrigatório' }).max(50),
-  email: z.string().trim().email({ message: 'Email inválido' }),
-  password: z
-    .string()
-    .trim()
-    .min(8, { message: 'Senha deve ter pelo menos 8 caracteres' }),
-})
+import SignUpForm from './_components/sign-up-form'
+import SingInForm from './_components/sing-in-form'
 
 const AuthenticationPage = () => {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof registerSchema>>({
-    resolver: zodResolver(registerSchema),
-    defaultValues: {
-      name: '',
-      email: '',
-      password: '',
-    },
-  })
-
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof registerSchema>) {
-    console.log(values)
-  }
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Tabs defaultValue="login" className="w-[400px]">
@@ -56,86 +12,10 @@ const AuthenticationPage = () => {
           <TabsTrigger value="register">Register</TabsTrigger>
         </TabsList>
         <TabsContent value="login">
-          <Card>
-            <CardHeader>
-              <CardTitle>Login</CardTitle>
-              <CardDescription>Faça login para continuar.</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2"></CardContent>
-            <CardFooter>
-              <Button>Login</Button>
-            </CardFooter>
-          </Card>
+          <SingInForm />
         </TabsContent>
         <TabsContent value="register">
-          <Card>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8"
-              >
-                <CardHeader>
-                  <CardTitle>Criar Conta</CardTitle>
-                  <CardDescription>
-                    Crie uma conta para continuar.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-4">
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Nome:</FormLabel>
-                        <FormControl>
-                          <Input placeholder="shadcn" {...field} />
-                        </FormControl>
-
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email:</FormLabel>
-                        <FormControl>
-                          <Input placeholder="shadcn" {...field} type="email" />
-                        </FormControl>
-
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Senha:</FormLabel>
-                        <FormControl>
-                          <Input
-                            placeholder="shadcn"
-                            {...field}
-                            type="password"
-                          />
-                        </FormControl>
-
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </CardContent>
-                <CardFooter>
-                  <Button type="submit" className="w-full">
-                    Criar Conta
-                  </Button>
-                </CardFooter>
-              </form>
-            </Form>
-          </Card>
+          <SignUpForm />
         </TabsContent>
       </Tabs>
     </div>
